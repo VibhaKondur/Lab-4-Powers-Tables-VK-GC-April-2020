7d3dcb6a189c5a648358bcf9e591be35a381904b
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-04-22 18:44:20 -0400

    Lab 4 Powers Table VK GC April 2020

---
+++ Lab 4 Powers Tables VK GC April 2020/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+
+namespace Lab_4_Powers_Tables_VK_GC_April_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool runAgain = true;
+
+            while (runAgain)
+            {
+            int number = 0;
+            int i = 0;
+            Console.WriteLine("Hello! Please enter an integer. ");
+                number = int.Parse(Console.ReadLine());
+
+                for (i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                for (i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i * i);
+                }
+                for (i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i * i * i);
+                }
+
+                Console.WriteLine("Great job! Continue? y/n");
+                string runAgainstring = Console.ReadLine();
+                if (runAgainstring == "y")
+                {
+                    runAgain = true;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+        }
+        
+    }
+}
